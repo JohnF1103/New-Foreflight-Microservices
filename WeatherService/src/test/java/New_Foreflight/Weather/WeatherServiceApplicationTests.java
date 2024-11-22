45c9fe8df53b8f1ef434c2cb6b14c880cb694691
JohnF1103 <87583884+JohnF1103@users.noreply.github.com>
2024-11-22 15:33:05 -0500

    WeatherService

---
+++ WeatherService/src/test/java/New_Foreflight/Weather/WeatherServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package New_Foreflight.Weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
