2778ec61eb17cd8bb0bbb18196cbb4d168af7488
JohnF1103 <87583884+JohnF1103@users.noreply.github.com>
2024-11-22 16:44:05 -0500

    test

--- WeatherService/src/main/java/New_Foreflight/Weather/WeatherServiceApplication.java
+++ WeatherService/src/main/java/New_Foreflight/Weather/WeatherServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WeatherServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(WeatherServiceApplication.class, args);
 	}
 
