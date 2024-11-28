971c3365a9ee5407d5bbbcdb1d40e4c19116edb1
JohnF1103 <87583884+JohnF1103@users.noreply.github.com>
2024-11-27 19:05:17 -0500

    Wx API call. parsing next.

--- WeatherService/src/main/java/New_Foreflight/Weather/Service/WeatherServiceImpl.java
+++ WeatherService/src/main/java/New_Foreflight/Weather/Service/WeatherServiceImpl.java
@@ -1,16 +1,28 @@
 package New_Foreflight.Weather.Service;
 
 import New_Foreflight.Weather.DTO.AirportInfoResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
 
 @Service
 public class WeatherServiceImpl implements Weatherservice {
 
+    @Value("${checkwx.api.url}")
+    private String apiUrl;
+
+    @Value("${checkwx.api.key}")
+    private String apiKey;
+
     @Override
-    public AirportInfoResponse getAirportInfo(String iaco) {
+    public AirportInfoResponse getAirportInfo(String icao) {
 
+        String endpoint = apiUrl.replace("{station}", icao) + "?x-api-key=" + apiKey;
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(endpoint, String.class);
 
-        return new AirportInfoResponse(ParseAirportInfo(iaco),"vfr");
+        return new AirportInfoResponse(ParseAirportInfo(icao), "vfr");
     }
 
     @Override
