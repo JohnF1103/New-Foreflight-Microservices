4fe695b561f5feaa5019faf17c11dcc5700c2c6f
JohnF1103 <johnefoster@icloud.com>
2024-12-03 23:51:33 -0500

    Wxservice getrawtext

--- WeatherService/src/main/java/New_Foreflight/Weather/Service/WeatherServiceImpl.java
+++ WeatherService/src/main/java/New_Foreflight/Weather/Service/WeatherServiceImpl.java
@@ -1,10 +1,17 @@
 package New_Foreflight.Weather.Service;
 
 import New_Foreflight.Weather.DTO.AirportInfoResponse;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 @Service
 public class WeatherServiceImpl implements Weatherservice {
@@ -21,15 +28,22 @@ public class WeatherServiceImpl implements Weatherservice {
         String endpoint = apiUrl.replace("{station}", icao) + "?x-api-key=" + apiKey;
         RestTemplate restTemplate = new RestTemplate();
         String result = restTemplate.getForObject(endpoint, String.class);
+    
 
-        return new AirportInfoResponse(ParseAirportInfo(icao), "vfr");
+        return new AirportInfoResponse(ParseAirportInfo(result), "vfr");
     }
 
     @Override
     public String ParseAirportInfo(String info) {
 
-        info = "call";
+        
+        
+        JSONObject json = new JSONObject(info);
 
-        return "getting for " + info;
+        Object result = json.getJSONArray("data").getJSONObject(0).get("raw_text");
+
+        System.out.println(result.toString());
+        return "getting for ";
+        
     }
 }
